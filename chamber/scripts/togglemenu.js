53e527d89539255207b90e9d5f922480d28330f2
annavarner <var21033@byui.edu>
2022-11-11 16:57:28 -0500

    W09 commit

--- chamber/scripts/togglemenu.js
+++ chamber/scripts/togglemenu.js
@@ -7,3 +7,18 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger")
 x.onclick = toggleMenu;
+
+// change view on directory page to grid or list
+const gridbutton = document.querySelector("#viewGrid");
+const listbutton = document.querySelector("#viewList");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid")
+    display.classList.remove("list");
+})
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+})
