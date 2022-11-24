6b0ba8ca97338b5276948269d2cbb0605e6c9348
annavarner <var21033@byui.edu>
2022-11-23 19:58:16 -0500

    fix javascript error

--- chamber/scripts/lastvisit.js
+++ chamber/scripts/lastvisit.js
@@ -8,8 +8,9 @@ let today = Date.now()
 //nearest whole number
 
 let milliseconds = (today - lastVisit);
-days = (milliseconds / 86,400,000)
-rounded = Math.round(days)
+let days = (milliseconds / 86,400,000)
+let rounded = Math.round(days)
+
 //change local storage value to today & display number of days since last visit
 localStorage.setItem("lastVisit", today);
-visits.innerHTML = `${rounded}`;
+visit.textContent = rounded;
