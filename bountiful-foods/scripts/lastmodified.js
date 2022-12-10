8f8b40452827b0e91be1760e0310baf4affa3ba0
annavarner <var21033@byui.edu>
2022-12-10 16:35:24 -0500

    main page med view

---
+++ bountiful-foods/scripts/lastmodified.js
@@ -0,0 +1,28 @@
+//Insert current year after copyright sign in footer
+
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector(".currentyear").textContent = year;
+
+
+//Insert last modified date in footer
+
+const lastMod = new Date(document.lastModified);
+
+const formatted = lastMod.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+});
+
+document.getElementById("lastupdated").innerHTML = formatted;
+
+//display number of days since last visit
+
+// const visits = document.querySelector("visit");
+// let lastVisit = Number(window.localStorage.getItem("visit-date"));
+// let today = Date.now()
+
