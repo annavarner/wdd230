0436ca20c4c0e841d6f246bf493664bd5c351a14
annavarner <var21033@byui.edu>
2022-12-12 20:55:59 -0500

    added about us, validated css & html

--- bountiful-foods/scripts/fruits.js
+++ bountiful-foods/scripts/fruits.js
@@ -10,7 +10,6 @@ async function apiFetch() {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data)
       data.forEach(displayFruit1);
       data.forEach(displayFruit2);
       data.forEach(displayFruit3);
@@ -26,30 +25,52 @@ async function apiFetch() {
 
   function displayFruit1(data) {
     let option = document.createElement('option');
-    console.log(option)
     option.textContent = data.name;
     fruit1.add(option);
-
-
   }
 
   function displayFruit2(data) {
     let option = document.createElement('option');
-    console.log(option)
     option.textContent = data.name;
     fruit2.add(option);
-
-  
-
   }
 
   function displayFruit3(data) {
     let option = document.createElement('option');
-    console.log(option)
     option.textContent = data.name;
     fruit3.add(option);
+  }
+
+  function outputInfo(){
+    const card = document.getElementById('output-card');
+    const name = document.createElement('h3');
+    const date = document.createElement('p');
+    const contact = document.createElement('p');
+    const ingredients = document.createElement('p');
+    const instructions = document.createElement('p');
+    const nutrition = document.createElement('ul');
+
+    card.appendChild(name);
+    card.appendChild(contact);
+    card.appendChild(ingredients);
+    card.appendChild(instructions);
+    card.appendChild(nutrition);
+    card.appendChild(date);
+
+
+    
+    name.innerHTML = `${document.getElementById('firstname').value}'s Specialty Drink`;
+    contact.innerHTML = `${document.getElementById('email').value} - ${document.getElementById('phone').value}`;
+
+    ingredients.innerHTML = `<strong>Ingredients:</strong> ${document.getElementById('fruit1').value}, 
+      ${document.getElementById('fruit2').value}, ${document.getElementById('fruit3').value}. `;
+    instructions.innerHTML = `<strong>Special Instructions:</strong> <em>${document.getElementById('instructions').value}</em>`;
+    nutrition.innerHTML = `Nutritional Information:`;
+    date.innerHTML = `Created ${new Date()}`;
 
 
   }
-
+  
   apiFetch();
+  const y = document.getElementById('submitBtn')
+  y.onclick = outputInfo;
