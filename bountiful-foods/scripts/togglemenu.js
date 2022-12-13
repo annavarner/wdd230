df78274dad5d1ba90602b82d82767f5d335d5518
annavarner <var21033@byui.edu>
2022-12-13 14:46:12 -0500

    working on fresh

--- bountiful-foods/scripts/togglemenu.js
+++ bountiful-foods/scripts/togglemenu.js
@@ -1,11 +1,16 @@
 
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("open");
-    document.getElementById("hamburger").classList.toggle("open");
+    document.querySelector(".hamburger").classList.toggle("open");
 
 }
+let menu = document.querySelector(".hamburger");
 
-const x = document.getElementById("hamburger")
+menu.addEventListener("click", () => {
+    menu.classList.toggle("clicked")
+});
+
+const x = document.querySelector(".hamburger")
 x.onclick = toggleMenu;
 
 // change view on directory page to grid or list
